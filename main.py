b680abf5e5c33e6e9184bdac14a22437b1d78006
nurulazealia <nurulazealia@gmail.com>
2020-12-30 02:48:48 +0800

    added a navbar, base template & database

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
