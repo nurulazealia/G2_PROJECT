db430d6e2b8a75a6bffc65fa13dc6957ff286d1a
nurulazealia <nurulazealia@gmail.com>
2020-12-30 12:04:45 +0800

    Edit the list file and database

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
-app.config['SECRET_KEY'] = "random string"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
+
+class Sounds(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Title %r>' %self.id
 
 @app.route("/")
 def home():
@@ -16,9 +23,25 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/lists")
+@app.route("/lists", methods = ['POST', 'GET'])
 def lists():
-    return render_template("lists.html")
+    db.create_all()
+    
+    if request.method == "POST":
+        sound_title = request.form['title']
+        new_sound = Sounds(title=sound_title)
+
+        try:
+            db.session.add(new_sound)
+            db.session.commit()
+            return redirect ('/lists')
+
+        except:
+            return "There was an error"
+
+    else:
+        sounds = Sounds.query.order_by(Sounds.date_uploaded)
+        return render_template("lists.html", sounds=sounds)
 
 if __name__ == "__main__":
     app.run(debug=True)
