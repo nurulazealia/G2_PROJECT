1bc03cc0d493bbb9bd06791dd89d98f046306fbe
nurulazealia <nurulazealia@gmail.com>
2021-01-05 18:46:23 +0800

    rename main.py to app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///127.0.0.1:5000/database'
 
 db = SQLAlchemy(app)
 
