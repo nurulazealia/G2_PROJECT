d758ab3ffcd825e6ca11d872ed8c4e7a0f1482cc
nurulazealia <nurulazealia@gmail.com>
2021-02-14 23:27:55 +0800

    edit the database

--- tables.py
+++ tables.py
@@ -10,6 +10,15 @@ class Results(Table):
     #edit = LinkCol('Rename', 'update', url_kwargs=dict(id='id'))
     delete = LinkCol('Delete', 'delete', url_kwargs=dict(id='id'))
 
+class Reports(Table):
+    id = Col('ID', show=True)
+    sound_path = Col('Sound Path')
+    waveform_path = Col('Waveform Path')
+    file_type = Col('File Type')
+    #show = LinkCol('View', 'show', url_kwargs=dict(id='id'))
+    #edit = LinkCol('Rename', 'update', url_kwargs=dict(id='id'))
+    #delete = LinkCol('Delete', 'delete', url_kwargs=dict(id='id'))
+
 class Explore(Table):
     title = Col('Filename')
     location = Col('Location')
